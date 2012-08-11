00b628e00b222ed051897008fb0457b4bf7e5ad0
Jan Buschtöns <buschtoens@gmail.com>
2012-08-11 03:44:13 +0200

    First running version

---
+++ src/com/github/cubixcraft/plugin/Plugin.java
@@ -0,0 +1,12 @@
+package com.github.cubixcraft.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+	public void onEnable() {
+		getLogger().info("Your plugin has been enabled.");
+	}
+	public void onDisable() {
+		getLogger().info("Your plugin has been enabled.");
+	}
+}
